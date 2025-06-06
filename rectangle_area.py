4fab36ef0db05c04dab6a05b4aadbf79e56cd28f
Makhutso Poto <mapotojhb024@student.wethinkcode.co.za>
2025-06-07 00:57:53 +0200

    first commit

---
+++ rectangle_area.py
@@ -0,0 +1,9 @@
+
+def rectangle_area(length,width):
+   area = length*width
+   return f"This is the area: {area}"
+
+
+
+   
+   
---
+++ test_rectangle_area.py
@@ -0,0 +1,14 @@
+import unittest
+from rectangle_area import*
+
+class TestCase(unittest.TestCase):
+    def test_small_numbers(self):
+        self.assertEqual(rectangle_area(2,3),"This is the area: 6")
+    def test_wrong_numbers(self):
+        self.assertNotEqual(rectangle_area(2,3),'6')
+    def test_big_numbers(self):
+        self.assertEqual(rectangle_area(100,50),'This is the area: 5000')
+    
+
+if __name__ == "__main__":
+    unittest.main()
