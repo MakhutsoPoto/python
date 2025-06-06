3725d33891d48cda1d2ef39222e4ef4dc35d247a
Makhutso Poto <mapotojhb024@student.wethinkcode.co.za>
2025-06-07 00:59:21 +0200

    created function for area of a rectangle,with tests that work .Proud

--- test_rectangle_area.py
+++ test_rectangle_area.py
@@ -8,7 +8,8 @@ class TestCase(unittest.TestCase):
         self.assertNotEqual(rectangle_area(2,3),'6')
     def test_big_numbers(self):
         self.assertEqual(rectangle_area(100,50),'This is the area: 5000')
-    
+    def test_int_numbers(self):
+        self.assertEqual(rectangle_area(5,12),"This is the area: 60")
 
 if __name__ == "__main__":
     unittest.main()
